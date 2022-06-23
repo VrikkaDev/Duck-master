62079183ee95d60240fd214a383f2ae2481e607e
vrikka hevonen <vrikkah@hotmail.com>
2022-06-23 17:05:09 +0300

    Initial commit

---
+++ src/main/java/net/VrikkaDuck/duck/Main.java
@@ -0,0 +1,13 @@
+package net.VrikkaDuck.duck;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("duck");
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
